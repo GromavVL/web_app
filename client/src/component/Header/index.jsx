b4e2abaaf3175cb9990f4fa5336200aaf47adc5a
Vlad <vlad086367@gmail.com>
2025-10-07 23:04:56 +0300

    Initial commit

---
+++ client/src/component/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Header.module.scss'
+
+function Header() {
+  return (
+    <header className={styles.headerHome}>Header</header>
+  )
+}
+
+export default Header;
