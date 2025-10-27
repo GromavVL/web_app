fdf393fdfecf444b926bac9d502aa4669bf5dc48
Vlad <vlad086367@gmail.com>
2025-10-27 21:29:09 +0200

    Add Catalog cart

--- client/src/pages/Catalog/index.jsx
+++ client/src/pages/Catalog/index.jsx
@@ -1,10 +1,116 @@
-import React from 'react'
-// import styles from './Catalog.module.scss'
+import React from 'react';
+import Asus from '../../assets/LaptopsImg/Asus/ASUSTUFGamingA14.png';
+import styles from './Catalog.module.scss';
 
 function Catalog() {
   return (
-    <div>Catalog</div>
-  )
+    <main className={styles.mainHome}>
+      <select className={styles.sortSelect}>
+        <option>Від дешевих до дорогих</option>
+        <option>Від дорогих до дешевих</option>
+        <option>За рейтингом</option>
+        <option>Новинки</option>
+      </select>
+      <section className={styles.sectionCart}>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+        <article className={styles.cartLaptop}>
+          <img className={styles.imgLaptop} src={Asus} alt="asus" />
+          <div className={styles.cartInfo}>
+            <p>Asus</p>
+            <p>TUF Gaming F15</p>
+          </div>
+          <p className={styles.elPrice}>1200 $</p>
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default Catalog;
