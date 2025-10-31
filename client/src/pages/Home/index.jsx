f2387a4a567aae7eae6a5cd8df9baebe65370bfe
Vlad <vlad086367@gmail.com>
2025-10-31 16:50:14 +0200

    Bug fix Home page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import styles from './Home.module.scss'
 import LaptopSlider from '../../component/LaptopSlider';
-import LaptopBrands from '../../component/LaptopBrands';
+import brands from '../../utils/validates';
+
 
 function Home() {
   return (
     <div>
       <LaptopSlider />
-      <LaptopBrands />
+      <section className={styles.laptopBrands}>
+      <h2 className={styles.title}>Популярний продукт</h2>
+      <div className={styles.brandsContainer}>
+        {brands.map((brand) => (
+          <article key={brand.name} className={styles.brands}>
+            <img className={styles.brandImage} src={brand.image} alt={brand.name} />
+            <h3 className={styles.brandName}>{brand.name}</h3>
+          </article>
+        ))}
+      </div>
+    </section>
     </div>
   );
 }
---
+++ client/src/utils/validates/index.js
@@ -0,0 +1,20 @@
+import Asus from '../../assets/LaptopsImg/Asus/ASUSTUFGamingA14.png';
+import Msi from '../../assets/LaptopsImg/Msi/RaiderA18HXA9W.png';
+import Lenovo from '../../assets/LaptopsImg/Lenovo/LenovoLegion5.png';
+import Gigabyte from '../../assets/LaptopsImg/Gigabyte/GIGABYTEGAMINGA16GA63H.png';
+import Acer from '../../assets/LaptopsImg/Acer/PredatorHelios16.png';
+
+
+
+
+
+
+const brands = [
+  { name: 'Asus', image: Asus },
+  { name: 'Msi', image: Msi },
+  { name: 'Lenovo', image: Lenovo },
+  { name: 'Gigabyte', image: Gigabyte },
+  { name: 'Acer', image: Acer },
+];
+
+export default brands;
